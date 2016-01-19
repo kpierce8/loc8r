aea034ca80c62273e1cc8b305916973aa990b161
Ken Pierce <kenpierce@Kens-MacBook-Pro.local>
2016-01-18 22:42:18 -0800

    mostly done chapter 5, need to add production database toggle and fix data driven views from end of 4

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,7 +2,34 @@
 
 
 module.exports.homeList = function(req, res){
-	res.render('locations-list', { title: 'Home' });
+	res.render('locations-list', { 
+
+		title: 'loc8r and a bunch of title text',
+		pageHeader: {
+			title: 'Loc8r',
+			strapline: 'Find places to leach bandwidth'
+		},
+		locations: [{
+			name: 'Olympia Cards and Comics',
+			address: 'near that circle in Lacey',
+			rating: 5,
+			facilities: ['Magic Cards', 'Superhero Stuff', 'Sci-Fi memorabilia'],
+			distance: 'close'
+		}, {
+			name: 'Barnes & Noble',
+			address: 'in Olympia next to Trader Joes',
+			rating: 3,
+			facilities: ['Magic Cards', 'Books', 'Coffee'],
+			distance: '10 km'
+		}, {
+			name: '5 Guys',
+			address: 'in front of Sport"s Authority',
+			rating: 4,
+			facilities: ['Snorkel Stuff', 'Iron', 'Shoes'],
+			distance: 'close'
+		    }
+		] 
+	});
 }
 
 module.exports.locationInfo = function(req, res){
--- app_server/models/main.js
+++ app_server/models/main.js
@@ -1,9 +1,9 @@
-module.exports.index = function(req, res){
-	res.render('index', { title: 'Express' });
-}
+// module.exports.index = function(req, res){
+// 	res.render('index', { title: 'Express' });
+// }
 
 
-//still here cause i left it
-var homepageController = function(req, res, next){
-	res.render('index', { title: 'Express' });
-}
+// //still here cause i left it
+// var homepageController = function(req, res, next){
+// 	res.render('index', { title: 'Express' });
+// }
